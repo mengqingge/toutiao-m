c768933dab2b1e6600fd3e4c97dc8a16d906d68f
mengqingge <1079125857@qq.com>
2022-07-18 23:58:08 +0800

    项目初始化，删除不用文件

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import './styles/index.less'
+// import '@/styles/index.less'
 Vue.config.productionTip = false
 
 new Vue({
+
+
   router,
   store,
   render: h => h(App)
